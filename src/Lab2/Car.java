3122f92ba634cbdade1000f90607f1748cb2f780
Draken62 <43519443+Draken62@users.noreply.github.com>
2018-09-24 16:52:37 +0100

    updated version2
    update

--- src/Lab2/Car.java
+++ src/Lab2/Car.java
@@ -1,37 +1,37 @@
 package Lab2;
 
 public class Car{
-    private String make; 	// Name of the student
-    private String model;	// Surname of the student
-    private int year;
-    private double cost;// Mark of the student
+    private String make; 	// make of the car
+    private String model;	// model of the car
+    private int year; // year of manufacture
+    private int  cost;// cost of the car
 
 // Constructor
-public Car(String firstName, String lastName, int i, int mark, double cost) {
-        this.make = firstName;
-        this.model = lastName;
+public Car(String make, String model, int mark, int cost) {
+        this.make = make;
+        this.model = model;
         this.year = mark;
         this.cost = cost;
         }
 
 public String toString() {
         // Returns name and result to be printed by UI
-        return make + " " + model + " received a " + resultName(year) + " for a mark of " + year;
+        return make + " " + model +  "  payed " + cost + " received a " + resultName(year) + " for a mark of " + year;
         }
 
-private String resultName(int mark) {
-        // Translates numeric result to their equivalent grade mark
-        String result = "";
+private String resultName(int rating) {
+        // Translates numeric result to their equivalent grade rating
+        String result;
 
-        if (mark >= 1 ) {
-        result = "shiny to new"; 	// Result above 85 gets Distinction
-        } else if (mark >= 3 && mark <= 6) {
-        result = "Merit";		// Result between 65 and 84 gets Merit
-        } else if (mark >= 6 && mark <= 8) {
+        if (rating >= 1 && rating <=3 ) {
+        result = "shiny to new"; 	// #rating one and two equals shiny to new
+        } else if (rating >= 3 && rating == 6) {
+        result = "Merit";		// Rating between 3 and 6 gets Merit
+        } else if (rating >= 6 && rating == 8) {
             result = "Pass";
-        }else if (mark >= 8 && mark <= 10) {
+        }else if (rating >= 8 && rating <= 10) {
             result = "Pass";
-            // Result between 40 and 64 gets Pass
+            // Rating between 8 and 10 gets Pass
         } else {
         result = "banger";		// Otherwise gets Fail
         }
@@ -39,7 +39,5 @@ private String resultName(int mark) {
         return result;
         }
 
-        // We don't create mutators or accessors as we don't want this class
-        // to be modified outside the Constructor
 
         }
